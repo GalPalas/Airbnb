0673668c040d9fc025f0c1ff0930aebd8b50cd1d
Gal Palas <galpalas89@gmail.com>
2022-12-25 20:03:16 +0200

    Add Explore Nearby Section

---
+++ data.js
@@ -0,0 +1,58 @@
+export const cities = [
+  {
+    id: "1",
+    image:
+      "https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTYyNDg1MjE3MTI1Mjc5Mzk4/topic-london-gettyimages-760251843-promo.jpg",
+    location: "London",
+    distance: "45-minutes drive",
+  },
+  {
+    id: "2",
+    image:
+      "https://www.shutterstock.com/image-photo/old-new-town-hall-buildings-600w-343479947.jpg",
+    location: "Manchester",
+    distance: "4.5-hour drive",
+  },
+  {
+    id: "3",
+    image:
+      "https://www.shutterstock.com/image-photo/panorama-bristol-station-square-600w-151657010.jpg",
+    location: "Bristol",
+    distance: "50-minutes drive",
+  },
+  {
+    id: "4",
+    image:
+      "https://www.shutterstock.com/image-photo/birmingham-canals-600w-419496859.jpg",
+    location: "Birmingham",
+    distance: "2-hour drive",
+  },
+  {
+    id: "5",
+    image:
+      "https://www.shutterstock.com/image-photo/aerial-close-tower-royal-liver-600w-2079532492.jpg",
+    location: "LIVERPOOL",
+    distance: "60-minutes drive",
+  },
+  {
+    id: "6",
+    image:
+      "https://www.shutterstock.com/image-photo/bradford-town-hall-west-yorkshire-600w-130153085.jpg",
+    location: "Bradford",
+    distance: "20-minutes drive",
+  },
+  {
+    id: "7",
+    image:
+      "https://www.shutterstock.com/image-photo/nottingham-council-house-front-shot-600w-399073087.jpg",
+    location: "Nottingham",
+    distance: "45-minutes drive",
+  },
+  {
+    id: "8",
+    image:
+      "https://www.shutterstock.com/image-photo/summer-sunset-hoe-plymouth-600w-1706031190.jpg",
+    location: "Plymouth",
+    distance: "4-hour drive",
+  },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
+import { cities } from "../data.js";
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +15,23 @@ const Home: NextPage = () => {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {cities.map(({ id, image, location, distance }) => (
+              <SmallCard
+                key={id}
+                img={image}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
